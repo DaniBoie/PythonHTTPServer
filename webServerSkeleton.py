eb38d26d82065d38e6b1f83779bdc8d40a04dfac
Daniel Ayala <dannyboy3385@gmail.com>
2023-05-03 10:09:27 -0700

    404 and 301 error messages

--- webServerSkeleton.py
+++ webServerSkeleton.py
@@ -29,8 +29,7 @@ while True:
         f = open(filename[1:], "rb")
         outputdata = f.read()
         
-        # Send one HTTP header line into socket
-        #Fill in start     
+        # Send HTTP header line into socket
         connectionSocket.send(b"HTTP/1.1 200 OK")
 
         if (filename.endswith(".jpg")):
@@ -39,35 +38,33 @@ while True:
         elif (filename.endswith(".gif")):
             print("GIF SENDING!")
             connectionSocket.send(b"\r\nContent-type: image/gif")
-            
-        
+
         connectionSocket.send(b"\r\n\r\n")
-        #Fill in end
         
         # Send the content of the requested file to the client
-        #Fill in start     
         connectionSocket.send(outputdata)
-        #Fill in end
         
         # Close client socket
-        #Fill in start     
         connectionSocket.close()
-        #Fill in end        
-    except IOError:
-        #handle the case if the file has been moved to a new location
-        #Fill in start
-        #Fill in end
+
+    except IOError as e:
+        errorFile = e.filename
         
-        #handle the case for file not found
-        #Fill in start
-        #Fill in end
         
-        # sample code for a response
-        connectionSocket.send(b'HTTP/1.1 426 Upgrade Required\r\n\r\n')
+        if (errorFile == "lab0.html"):
+            #handle the case if the file has been moved to a new location
+            connectionSocket.send(b"HTTP/1.1 301 Moved Permanently\r\n\r\n")
+            connectionSocket.send(b'<html><head></head><body><h1>lab0.html has been moved to lab2.html</h1></body></html>')
+        else:
+            #handle the case for file not found
+            connectionSocket.send(b"HTTP/1.1 404 Not Found\r\n\r\n")
+            connectionSocket.send(b'<html><head></head><body><h1>File Requested not found</h1></body></html>')
+
+        # connectionSocket.send(b'HTTP/1.1 426 Upgrade Required\r\n\r\n')
         # connectionSocket.send(b'<html><head></head><body><h1>This service requires use of the HTTP/1.1 protocol</h1></body></html>')
         
         # Close client socket
-        # connectionSocket.close()
+        connectionSocket.close()
     except KeyboardInterrupt:
         # User pressed Ctrl+C, exit gracefully
         break
